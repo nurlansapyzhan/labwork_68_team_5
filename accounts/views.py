6cef3aa69db567bd572d0367e6577eef0ee958c4
Kudaibergen Kyrgyzbay <kyrgyzbay.kudaibergen@gmail.com>
2023-04-13 05:51:29 +0600

    #2 Функционал аутентификации Добавил функционалы детального просмотра данных пользователя и смену пароля

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,10 @@
-from django.contrib.auth import authenticate, login, logout
-from django.views.generic import TemplateView, CreateView
+from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse
+from django.views.generic import TemplateView, CreateView, DetailView, UpdateView
 from django.shortcuts import redirect
 
-from .forms import LoginForm, CustomUserCreationForm
+from .forms import LoginForm, CustomUserCreationForm, PasswordChangeForm
 
 
 class LoginApplicantView(TemplateView):
@@ -48,8 +50,24 @@ class RegisterView(CreateView):
         form = self.form_class(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
-            user.save()
             login(request, user)
             return redirect(self.success_url)
         context = {'form': form}
         return self.render_to_response(context)
+
+class ProfileView(LoginRequiredMixin, DetailView):
+    model = get_user_model()
+    template_name = 'user_detail.html'
+    context_object_name = 'user_obj'
+
+class UserPasswordChangeView(UserPassesTestMixin, UpdateView):
+    model = get_user_model()
+    template_name = 'user_password_change.html'
+    form_class = PasswordChangeForm
+    context_object_name = 'user_obj'
+
+    def test_func(self):
+        return self.request.user == self.get_object()
+
+    def get_success_url(self):
+        return reverse('login_applicant')
